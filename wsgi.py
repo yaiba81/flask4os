bbf2d6cc61aa40819863f98a92ac25c9261f5a13
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-26 00:46:50 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,13 @@ def hello():
         print('will connect')
         cx.connect("oe/oracle@myserver1.test.com:1521/orc1")
         res = "Hello World"
-    except Exception as e:        
-        res = 'error occured: ', e
-        print(res)
+    except cx_Oracle.DatabaseError as exc:
+        print(str(type(exc)))                      # <class 'cx_Oracle.NotSupportedError'>
+        print(repr(exc))                           # NotSupportedError('Variable_TypeByValue(): unhandled data type dict',)
+        error, = exc.args                          # "error" is a str, NOT a cx_Oracle._Error object
+        print("Oracle-Error-Code:", error.code)    # AttributeError: 'str' object has no attribute 'code'
+        print("Oracle-Error-Message:", error.message)
+        res = str(error.message)
     return res
 
 if __name__ == "__main__":
