d1bbd71c40f80c5999980b17fc7c2d03b2801336
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-25 21:56:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import os
 
 os.system("curl -s -o icbl.zip https://download.oracle.com/otn_software/linux/instantclient/195000/instantclient-basiclite-linux.x64-19.5.0.0.0dbru.zip && \
     unzip icbl.zip && \
-    cd instantclient_19_5 && \
-    export LD_LIBRARY_PATH=$(pwd) && \
-    rm icbl.zip")
+    cp -r instantclient_19_5 /opt/app-root/bin/instantclient_19_5")
+
+os.system("cd /opt/app-root/bin && \
+    wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libaio-0.3.109-13.el7.x86_64.rpm && \
+    rpm2cpio libaio-0.3.109-13.el7.x86_64.rpm | cpio -idmv")
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
