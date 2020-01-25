717c54ea234a39aae6ac6900c1798d806d6b3ae3
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-25 23:46:47 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import cx_Oracle as cx
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    res = cx.connect("oe/oracle@myserver1.test.com:1521/orc1")
+    return "Hello World!", res
 
 if __name__ == "__main__":
     application.run()
