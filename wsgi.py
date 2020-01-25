972260b917f84ba4a007111ac28212999e0988dd
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-26 00:10:44 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,10 +6,12 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     try:
-        if cx.connect("oe/oracle@myserver1.test.com:1521/orc1"):
-            res = "Hello World"
-    except:
-        res = sys.exc_info()[0]
+        print('will connect')
+        cx.connect("oe/oracle@myserver1.test.com:1521/orc1")
+        res = "Hello World"
+    except:        
+        res = sys.exc_info()
+        print(res)
     return res
 
 if __name__ == "__main__":
