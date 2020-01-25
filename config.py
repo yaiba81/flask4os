bf3a45def5b3f6c249d1ffccffef5b6c7f8f12fc
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-25 20:55:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
+os.system("curl -s -o icbl.zip https://download.oracle.com/otn_software/linux/instantclient/195000/instantclient-basiclite-linux.x64-19.5.0.0.0dbru.zip && \
+    unzip icbl.zip && \
+    cd instantclient_19_5 && \
+    export LD_LIBRARY_PATH=$(pwd) && \
+    rm icbl.zip")
+
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
