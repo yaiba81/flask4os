2d35858b00899611becd8f3006934af1591f5c41
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-01-26 01:18:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-os.system("curl -s -o icbl.zip https://download.oracle.com/otn_software/linux/instantclient/195000/instantclient-basiclite-linux.x64-19.5.0.0.0dbru.zip && \
+os.system("./envi.sh")
+
+'''os.system("curl -s -o icbl.zip https://download.oracle.com/otn_software/linux/instantclient/195000/instantclient-basiclite-linux.x64-19.5.0.0.0dbru.zip && \
     unzip icbl.zip")
 os.system("wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libaio-0.3.109-13.el7.x86_64.rpm && \
     rpm2cpio libaio-0.3.109-13.el7.x86_64.rpm | cpio -idmv")
@@ -10,7 +12,7 @@ os.system("cd  /opt/app-root/src/instantclient_19_5 && \
     ln -s libaio.so.1.0.1 libaio.so.1")
 os.system("source /opt/app-root/etc/generate_container_user")
 os.system("cd /opt/app-root/src/instantclient_19_5 && \
-    export LD_LIBRARY_PATH=/opt/app-root/src/instantclient_19_5:$LD_LIBRARY_PATH")
+    export LD_LIBRARY_PATH=/opt/app-root/src/instantclient_19_5:$LD_LIBRARY_PATH")'''
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
