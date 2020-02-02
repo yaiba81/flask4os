2b85089cef481afedf5311fc471e83031c256c89
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-02-02 20:46:26 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,10 @@
 from flask import Flask
-import cx_Oracle as cx
-import os
 
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    print('about to connect')
-    try:        
-        cx.connect("oe/oracle@myserver1.test.com:1521/orc1")
-        res = "Hello World"
-    except cx_Oracle.DatabaseError as exc:
-        print(str(type(exc)))                      # <class 'cx_Oracle.NotSupportedError'>
-        print(repr(exc))                           # NotSupportedError('Variable_TypeByValue(): unhandled data type dict',)
-        error = exc.args                          # "error" is a str, NOT a cx_Oracle._Error object
-        print("Oracle-Error-Code:", str(error.code))   # AttributeError: 'str' object has no attribute 'code'
-        print("Oracle-Error-Message:", str(error.message))
-        res = str(error.message)
-    return res
+def hello():    
+    return "Hello"
 
 if __name__ == "__main__":
     application.run()
