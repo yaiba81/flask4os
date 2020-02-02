2b508581892fbfe5ceb447df2d94902d262a305b
yaiba81 <47979965+yaiba81@users.noreply.github.com>
2020-02-02 21:10:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
+os.system('GUNICORN_CMD_ARGS="--timeout=60"')
+
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-os.system('gunicorn -t 60 wsgi.py')
+
+
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
